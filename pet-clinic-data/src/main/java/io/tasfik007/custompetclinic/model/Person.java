fd77ef6b27ba1be13ed83e5fc9dfcc491480c4dd
Tasfik Rahman <tasfik.rahman@dsinnovators.com>
2021-06-28 15:28:29 +0600

    Added base entity. Closes #10

--- pet-clinic-data/src/main/java/io/tasfik007/custompetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/io/tasfik007/custompetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package io.tasfik007.custompetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/io/tasfik007/custompetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/io/tasfik007/custompetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package io.tasfik007.custompetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
