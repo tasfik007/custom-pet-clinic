1f11f6375716c38ae85d2406ea8e527d49d05456
Tasfik Rahman <tasfik.rahman@dsinnovators.com>
2021-07-24 11:14:58 +0600

    added list of owners and updated owner controller. Closes #18

--- pet-clinic-web/src/main/java/io/tasfik007/custompetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/io/tasfik007/custompetclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package io.tasfik007.custompetclinic.controllers;
 
+import io.tasfik007.custompetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
